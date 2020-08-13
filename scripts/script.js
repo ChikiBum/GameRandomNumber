2d8cc12ff1beefb58a399c097230493f19e6e5fc
Alexander Bozhko <obozhko82@gmail.com>
2020-08-14 00:55:42 +0300

    Init Lesson6

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//проверка что на входе число
+const isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const random = function (min, max) {
+    return Math.random() * (max - min) + min;
+  };
+
+  // console.log(Math.floor(random(1, 100)));
+
+const randomFloor = Math.floor(random(1, 100));
+
+  function compareNumbers(num){
+    let number = prompt('Угадай число от 1 до 100');
+    if (number === null){
+        return 'GAME OVER';
+    }
+    if (!isNumber(number)){
+        alert('Введи число!');
+        compareNumbers(num);
+     }
+    else if (+number < num){
+        alert('Загаданное число меньше');
+        compareNumbers(num);
+    } else if (+number > num){
+        alert('Загаданное число больше');
+        compareNumbers(num);
+    }
+  }
+
+  console.log(compareNumbers(randomFloor));
