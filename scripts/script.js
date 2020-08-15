a5edc43dba156787f3e5aedc80c95f6922dbaecf
Alexander Bozhko <obozhko82@gmail.com>
2020-08-15 14:36:37 +0300

    did hard

--- scripts/script.js
+++ scripts/script.js
@@ -5,34 +5,62 @@ const isNumber = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+
 const random = function (min, max) {
     return Math.random() * (max - min) + min;
   };
 
   // console.log(Math.floor(random(1, 100)));
 
-const randomFloor = Math.floor(random(1, 100));
+  function compareNumbers(){
+    
+    const num = Math.floor(random(1, 100));
+    let count = 0;
 
+    console.log(count);
+    console.log(num);
+    
+    return (function repeat(){
+        count++;
+        // console.log(count);
+          
+        const number = prompt('Угадай число от 1 до 100');
 
-  function compareNumbers(num){
-    let number = prompt('Угадай число от 1 до 100');
-    if (number === null){
-        alert('GAME OVER');
-    } 
-    else if (!isNumber(number)){
-        alert('Введи число!');
-        compareNumbers(num);
-     }
-    else if (+number < num){
-        alert('Загаданное число меньше');
-        compareNumbers(num);
-    } else if (+number > num){
-        alert('Загаданное число больше');
-        compareNumbers(num);
-    }
-    else if (+number === num){
-      alert('YOU ARE WIN!!');
-   }
+        if (count <= 10){
+
+        if (number === null){
+            alert('GAME OVER');
+            return;
+        } 
+        else if (!isNumber(number)){
+            alert('Введи число!');
+            
+        }
+        else if (+number < num){
+            alert(`Загаданное число меньше, осталось попыток ${10 - count}`);
+            
+        } else if (+number > num){
+            alert(`Загаданное число больше, осталось попыток ${10 - count}`);
+        }
+        else if (+number === num){
+          const confirmRes = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+          if (confirmRes){
+            compareNumbers(num);
+          } else {
+          return;
+          }
+      }
+      repeat();
+    } else {
+          const confirmRes = confirm('Попытки закончились, хотите сыграть еще?');
+          if (confirmRes){
+            compareNumbers(num);
+          } else {
+          return;
+          }
+      }
+  
+      })();
   }
 
-  console.log(compareNumbers(randomFloor));
+ compareNumbers();
